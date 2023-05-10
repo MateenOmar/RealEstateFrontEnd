511e9941910f7977d94899b89ae8d0a91e0c6b4a
Omar <omar.mateen@soti.net>
2023-05-10 09:46:20 -0400

    connected api

--- src/app/property/add-property/add-property.component.ts
+++ src/app/property/add-property/add-property.component.ts
@@ -28,7 +28,7 @@ export class AddPropertyComponent implements OnInit {
 
   propertyTypes: Array<string> = ['House', 'Apartment', 'Duplex'];
   furnishTypes: Array<string> = ['Fully', 'Semi', 'Unfurnished'];
-  cityList!: string[];
+  cityList!: any[];
 
   propertyView: IPropertyBase = {
     ID: null!,
